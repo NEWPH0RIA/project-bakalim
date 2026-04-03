f84caa6c83e9e111d2c4fc9f715ee2c7ba019999
NEWPH0RIA <newph0ria@icloud.com>
2026-04-03 11:34:43 +0300

    Delete src directory

--- src/views/articles/index.php
+++
@@ -1,7 +0,0 @@
-<h1><//h1>
-<?php foreach($articles as $article): ?>
-    <h1><?= $article->getName()?></h1>
-    <h1><?= $article->getText() ?></h1>
-    <p>Автор: <?= $article->getAuthor()->getNickname() ?></p>
-    <a href="article/<?= 2 ?>"> Подробнее</a>
-<?php endforeach; ?>
--- src/views/articles/view.php
+++
@@ -1,3 +0,0 @@
-<h1><?= $article->getName()?></h1>
-<h1><?= $article->getText() ?></h1>
-<p>Автор: <?= $article->getAuthor()->getNickname() ?></p>
